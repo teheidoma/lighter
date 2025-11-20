1b20d950ea8f7a6c8a5322dffb5baeed98927517
teheidoma <teheidoma@gmail.com>
2025-11-20 11:49:45 +0200

    commit

--- src/main/java/com/teheidom/lighter/MainController.java
+++ src/main/java/com/teheidom/lighter/MainController.java
@@ -1,20 +1,46 @@
 package com.teheidom.lighter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.stream.Collectors;
+import com.teheidom.lighter.Schedule.Queue;
 
-import com.teheidom.lighter.provider.KiroeProvider;
+import com.teheidom.lighter.provider.Provider;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class MainController {
-  private final KiroeProvider kiroeProvider;
+  private final List<Provider> providers;
 
-  @GetMapping(value = "/", produces = "application/json")
-  public Object home() {
-    return kiroeProvider.getData();
+  @GetMapping(value = "/list", produces = "application/json")
+  public List<String> home() {
+    return providers.stream()
+        .map(Provider::getName)
+        .collect(Collectors.toList());
+  }
+
+  @GetMapping(value = "/{id}", produces = "application/json")
+  public Object home(@PathVariable String id) {
+    return providers.stream()
+        .filter(provider -> provider.getName().equals(id))
+        .findFirst()
+        .map(Provider::getData)
+        .orElseThrow();
+  }
+
+  @GetMapping(value = "/remaining/{id}/{group}")
+  public Object getRemaining(@PathVariable String id, @PathVariable String group) {
+    return providers.stream()
+        .filter(provider -> provider.getName().equals(id))
+        .findFirst()
+        .map(provider -> {
+          provider.getData().cast(Schedule.class);
+        }
+        .orElseThrow();
   }
 
 }
--- src/main/java/com/teheidom/lighter/provider/KiroeProvider.java
+++ src/main/java/com/teheidom/lighter/provider/KiroeProvider.java
@@ -1,17 +1,50 @@
 package com.teheidom.lighter.provider;
 
+import java.util.List;
+import java.util.regex.Pattern;
+
 import org.jsoup.Jsoup;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.stereotype.Component;
 
+import com.teheidom.lighter.Schedule.Queue;
+
 import lombok.SneakyThrows;
 
 @Component
-public class KiroeProvider extends TelegramProvider {
+public class KiroeProvider implements Provider {
   private static final String CHANNEL_NAME = "SvitloKropyvnytskyiMisto";
+  private String lastHeader = "";
+  private List<Queue> lastQueues = null;
+  private static final Pattern MAIN_PATTERN = Pattern.compile("Черга (\\d.\\d\\:(?:\\W\\d{2}-\\d{2},?)+)");
 
-  public KiroeProvider(OllamaChatModel chat) {
-    super(chat, CHANNEL_NAME);
+  @Override
+  public String getName() {
+    return "KiroeProvider";
+  }
+
+  @Override
+  @SneakyThrows
+  public Object getData() {
+    var doc = Jsoup.connect("https://kiroe.com.ua")
+        .timeout(5_000)
+        .get();
+    var header = doc.select(".fancybox_header > span").text();
+    if (header.equals(lastHeader)) {
+      return lastQueues;
+    } else {
+      lastHeader = header;
+      var body = doc.select(".fancybox_body_desc").text();
+      var queues = MAIN_PATTERN.matcher(body).results()
+          .map(match -> match.group(1))
+          .map(entry -> new Queue(
+              entry.split(":")[0],
+              List.of(entry.substring(entry.indexOf(":") + 1).replaceAll("\\s", "").split(","))))
+          .toList();
+
+      lastQueues = queues;
+      return queues;
+    }
   }
 
 }
